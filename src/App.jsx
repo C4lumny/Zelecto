f4e015c764a300fef9c64d1d6fe9617224e85a8c
C4lumny <ospinonathan@gmail.com>
2023-10-25 11:37:10 -0500

    Header listo

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ export function App() {
   return (
     <>
       <Header />
+      <div className=" mt-14 flex justify-center items-center gap-2">
+        <img src="src/assets/cubiertos.svg" alt="" className="w-14"/>
+        <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-black to-purple-400">RECETAS</h1>
+      </div>
     </>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,17 @@
-export function Header(){
-    return(
-        <header className='bg-black text-center p-7'>
-            <nav className='flex justify-center'>
-                <a href='#' className='hover-fuchsia text-white font-bold mx-8 hover:text-fuchsia-400 transition duration-200'>INICIO</a>
-                <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>EJERCICIOS</a>
-                <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>ALIMENTOS</a>
-                <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>TIENDA</a>
-            </nav>
-        </header>
+export function Header() {
+    return (
+      <header className='bg-black text-center p-5 flex items-center justify-between'>
+        <div className='flex items-center'>
+          <img src="src/assets/logo.png" alt="" className="h-24" />
+          <nav className='flex justify-center items-center'>
+            <a href='#' className='hover-fuchsia text-white font-bold mx-8 hover:text-fuchsia-400 transition duration-200'>INICIO</a>
+            <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>EJERCICIOS</a>
+            <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>ALIMENTOS</a>
+            <a href='#' className='text-white mx-8 hover:text-fuchsia-400 font-bold transition duration-200'>TIENDA</a>
+          </nav>
+        </div>
+        <img src="src/assets/user2.png" alt="" className="h-24" />
+      </header>
     );
-}
+  }
+  
